872ebbaa1d611dd6e2be90e70875bebb7a4ed804
Joma <117911315+CalvinAX@users.noreply.github.com>
2025-01-12 22:14:43 +0100

    Update algorithm.py

--- algorithm.py
+++ algorithm.py
@@ -50,9 +50,6 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 #model.fit(X_train, y_train)
 
 ### Random Forest Regressor
-model = RandomForestRegressor(n_estimators=100, random_state=42)
-model.fit(X_train, y_train)
-
 # Hyperparameter-Grid definieren
 # Einstellungen für das finden von besten Einstellungen für den RandomForestRegressor
 param_grid = {
